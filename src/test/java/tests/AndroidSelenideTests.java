9b0afcbdaad50ce8cafdeeb99f63f32361b17d1a
DaymianDark <nightkonn@gmail.com>
2022-04-11 16:30:19 +0300

    steps added

--- src/test/java/tests/AndroidSelenideTests.java
+++ src/test/java/tests/AndroidSelenideTests.java
@@ -1,20 +1,26 @@
 package tests;
 
 import io.appium.java_client.MobileBy;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
 import static com.codeborne.selenide.Selectors.byClassName;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
+import static io.qameta.allure.Allure.step;
 
 public class AndroidSelenideTests extends TestBase {
 
     @Test
+    @Tag("mobile")
     void searchTest() {
-        $(MobileBy.AccessibilityId("Search Wikipedia")).click();
-        $(MobileBy.id("org.wikipedia.alpha:id/search_src_text")).setValue("BrowserStack");
-        $$(byClassName("android.widget.TextView")).shouldHave(sizeGreaterThan(0));
-
+        step("type search", () -> {
+            $(MobileBy.AccessibilityId("Search Wikipedia")).click();
+            $(MobileBy.id("org.wikipedia.alpha:id/search_src_text")).setValue("BrowserStack");
+        });
+        step("verify content found", () -> {
+            $$(byClassName("android.widget.TextView")).shouldHave(sizeGreaterThan(0));
+        });
     }
 }
