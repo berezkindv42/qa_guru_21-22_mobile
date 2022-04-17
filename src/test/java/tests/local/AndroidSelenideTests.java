debfc3440274b0b13dc423a99712c9b10d8761f0
DaymianDark <nightkonn@gmail.com>
2022-04-17 12:18:29 +0300

    any tests added

--- src/test/java/tests/local/AndroidSelenideTests.java
+++ src/test/java/tests/local/AndroidSelenideTests.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.size;
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.CollectionCondition.texts;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
@@ -69,28 +70,89 @@ public class AndroidSelenideTests extends TestBase {
                         .shouldHave(sizeGreaterThan(0)));
     }
 
-        @Test
-        @Tag("local")
-        void customizeFeedHideAllOptionsTest() {
-            step("Skip onboarding pages", () -> {
-                    $(AppiumBy.id("org.wikipedia.alpha:id/fragment_onboarding_skip_button")).click();
-                    $(AppiumBy.id("org.wikipedia.alpha:id/view_announcement_action_negative")).click();
-            });
-            step("GO to feed customize menu", () -> {
-                    $(AppiumBy.id("org.wikipedia.alpha:id/nav_more_container")).click();
-                    $(AppiumBy.id("org.wikipedia.alpha:id/main_drawer_settings_container")).click();
-                    $$(AppiumBy.id("android:id/title")).findBy(text("Explore Feed")).click();
-            });
-            step("Hide all options", () -> {
-                $(AppiumBy.accessibilityId("More options")).click();
-                $$(AppiumBy.id("org.wikipedia.alpha:id/title")).findBy(text("Hide all")).click();
-                $(AppiumBy.className("android.widget.ImageButton")).click();
-                $(AppiumBy.className("android.widget.ImageButton")).click();
-            });
-            step("Check empty feed", () ->
-                $$(AppiumBy.className("android.widget.TextView")).filterBy(text("There's nothing on your Explore feed")).shouldHave(size(1)));
-
+    @Test
+    @Tag("local")
+    void customizeFeedHideAllOptionsTest() {
+        step("Skip onboarding pages", () -> {
+            $(AppiumBy.id("org.wikipedia.alpha:id/fragment_onboarding_skip_button")).click();
+            $(AppiumBy.id("org.wikipedia.alpha:id/view_announcement_action_negative")).click();
+        });
+        step("GO to feed customize menu", () -> {
+            $(AppiumBy.id("org.wikipedia.alpha:id/nav_more_container")).click();
+            $(AppiumBy.id("org.wikipedia.alpha:id/main_drawer_settings_container")).click();
+            $$(AppiumBy.id("android:id/title")).findBy(text("Explore Feed")).click();
+        });
+        step("Hide all options", () -> {
+            $(AppiumBy.accessibilityId("More options")).click();
+            $$(AppiumBy.id("org.wikipedia.alpha:id/title")).findBy(text("Hide all")).click();
+            $(AppiumBy.className("android.widget.ImageButton")).click();
+            $(AppiumBy.className("android.widget.ImageButton")).click();
+        });
+        step("Check empty feed", () ->
+                $$(AppiumBy.className("android.widget.TextView")).filterBy(text("There's nothing on your Explore feed"))
+                        .shouldHave(size(1)));
     }
 
+    @Test
+    @Tag("local")
+    void changeLanguageTest() {
+        step("Skip onboarding pages", () -> {
+            $(AppiumBy.id("org.wikipedia.alpha:id/fragment_onboarding_skip_button")).click();
+            $(AppiumBy.id("org.wikipedia.alpha:id/view_announcement_action_negative")).click();
+        });
+        step("GO to language menu", () -> {
+            $(AppiumBy.id("org.wikipedia.alpha:id/nav_more_container")).click();
+            $(AppiumBy.id("org.wikipedia.alpha:id/main_drawer_settings_container")).click();
+            $$(AppiumBy.id("android:id/title")).findBy(text("Wikipedia languages")).click();
+        });
+        step("Select russian language", () -> {
+            $$(AppiumBy.id("org.wikipedia.alpha:id/wiki_language_title"))
+                    .findBy(text("ADD LANGUAGE")).click();
+            $$(AppiumBy.id("org.wikipedia.alpha:id/localized_language_name"))
+                    .findBy(text("Русский")).click();
+        });
+        step("Check language added", () ->
+                $$(AppiumBy.className("android.widget.TextView")).filterBy(text("Русский"))
+                        .shouldHave(size(1)));
+    }
+
+    @Test
+    @Tag("local")
+    void mainMenuTest() {
+        step("Skip onboarding pages", () -> {
+            $(AppiumBy.id("org.wikipedia.alpha:id/fragment_onboarding_skip_button")).click();
+            $(AppiumBy.id("org.wikipedia.alpha:id/view_announcement_action_negative")).click();
+        });
+        step("Check 'Saved' page", () -> {
+            $$(AppiumBy.id("org.wikipedia.alpha:id/navigation_bar_item_small_label_view"))
+                    .findBy(text("Saved")).click();
+            $$(AppiumBy.className("android.widget.TextView")).filterBy(text("Saved"))
+                    .shouldHave(sizeGreaterThan(0));
+        });
+        step("Check 'Search' page", () -> {
+            $$(AppiumBy.id("org.wikipedia.alpha:id/navigation_bar_item_small_label_view"))
+                    .findBy(text("Search")).click();
+            $$(AppiumBy.className("android.widget.TextView")).filterBy(text("Search"))
+                    .shouldHave(sizeGreaterThan(0));
+        });
+        step("Check 'Edits' page", () -> {
+            $$(AppiumBy.id("org.wikipedia.alpha:id/navigation_bar_item_small_label_view"))
+                    .findBy(text("Edits")).click();
+            $$(AppiumBy.className("android.widget.TextView")).filterBy(text("Edits"))
+                    .shouldHave(sizeGreaterThan(0));
+        });
+        step("Check 'More' page", () -> {
+            $(AppiumBy.id("org.wikipedia.alpha:id/nav_more_container")).click();
+            $$(AppiumBy.className("android.widget.TextView"))
+                    .shouldHave(texts("LOG IN / JOIN WIKIPEDIA", "Settings", "Donate"));
+            back();
+        });
+        step("Check 'Explore' page", () -> {
+            $$(AppiumBy.id("org.wikipedia.alpha:id/navigation_bar_item_small_label_view"))
+                    .findBy(text("Explore")).click();
+            $$(AppiumBy.id("org.wikipedia.alpha:id/view_card_header_title"))
+                    .shouldHave(sizeGreaterThan(0));
+        });
+    }
 }
 
